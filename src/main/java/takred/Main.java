f48473d6b02bbcecd77a1dff1c6196bb865c554b
takred <sazonov.kos@yandex.ru>
2020-05-23 19:39:51 +0300

    Переделан мето для вчисления размера отряда.

--- src/main/java/takred/Main.java
+++ src/main/java/takred/Main.java
@@ -4,41 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
-//    public static List<AbundanceConfiguration> graduationTitleSquadSize() {
-//        List<AbundanceConfiguration> titleList = new ArrayList<>();
-//        titleList.add(new AbundanceConfiguration("Мало", 10));
-//        titleList.add(new AbundanceConfiguration("Нормально", 30));
-//        titleList.add(new AbundanceConfiguration("Толпа", 50));
-//        titleList.add(new AbundanceConfiguration("Свора", 100));
-//        titleList.add(new AbundanceConfiguration("Орда", 500));
-//        titleList.add(new AbundanceConfiguration("Легион", 2000));
-//        return titleList;
-//    }
-
     public static String a(int squadSize, List<AbundanceConfiguration> titleList) {
-        int index = 0;
-        if (squadSize < titleList.get(index).getSquadSize()) {
-            return titleList.get(index).getTitle();
+        for (int i = 0; i < titleList.size(); i++) {
+            if (squadSize < titleList.get(i).getSquadSize()) {
+                return titleList.get(i).getTitle();
+            }
         }
-        index = index + 1;
-        if (squadSize < titleList.get(index).getSquadSize()) {
-            return titleList.get(index).getTitle();
-        }
-        index = index + 1;
-        if (squadSize < titleList.get(index).getSquadSize()) {
-            return titleList.get(index).getTitle();
-        }
-        index = index + 1;
-        if (squadSize < titleList.get(index).getSquadSize()) {
-            return titleList.get(index).getTitle();
-        }
-        index = index + 1;
-        if (squadSize < titleList.get(index).getSquadSize()) {
-            return titleList.get(index).getTitle();
-        }
-        index = index + 1;
-        return titleList.get(index).getTitle();
+        return "Какая-то фигня.";
     }
 
     public static void main(String[] args) {
