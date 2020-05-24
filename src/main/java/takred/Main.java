3597ab27e0701a7921abc27db5a183f166ceca06
takred <sazonov.kos@yandex.ru>
2020-05-24 14:46:44 +0300

    Переименован метод titleSquadSize в метод getTitleBySquadSize.

--- src/main/java/takred/Main.java
+++ src/main/java/takred/Main.java
@@ -3,7 +3,7 @@ package takred;
 import java.util.List;
 
 public class Main {
-    public static String titleSquadSize(int squadSize, List<AbundanceConfiguration> titleList) {
+    public static String getTitleBySquadSize(int squadSize, List<AbundanceConfiguration> titleList) {
         for (int i = 0; i < titleList.size(); i++) {
             AbundanceConfiguration abundanceConfiguration = titleList.get(i);
             if (squadSize < abundanceConfiguration.getSquadSize()) {
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         GraduationTitleSquadSize titleList = new GraduationTitleSquadSize();
         int squadSize = 23;
-        String title = titleSquadSize(squadSize, titleList.getAbundanceConfigurations());
+        String title = getTitleBySquadSize(squadSize, titleList.getAbundanceConfigurations());
         System.out.println(title);
     }
 }
